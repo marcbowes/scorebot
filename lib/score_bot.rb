4b5dfa74dc106847d8da2f4d96af4c0ba351e87e
Marc Bowes <marc@marcb-laptop.(none)>
2008-12-25 16:38:58 +0200

    tidying up

--- lib/score_bot.rb
+++ lib/score_bot.rb
@@ -1,2 +1,8 @@
-require 'score_bot/server'
+# require farm for score_bot lib
+
+# add to ruby search path.. makes things easier
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
 require 'score_bot/incoming_message_interpreter'
+require 'score_bot/server'
--- lib/score_bot/server.rb
+++ lib/score_bot/server.rb
@@ -1,11 +1,6 @@
 require 'gserver'
 
 module ScoreBot
-  $:.unshift(File.dirname(__FILE__)) unless
-    $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-  
-  require 'incoming_message_interpreter'
-  
   include IncomingMessageInterpreter
   
   class Server < GServer
